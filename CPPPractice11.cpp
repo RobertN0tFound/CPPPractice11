61d966426f27994c44c2985931200d76cd965510
student <student@C246U14>
2022-11-29 14:10:39 +0300

    patch0.1

--- CPPPractice11.cpp
+++ CPPPractice11.cpp
@@ -13,43 +13,6 @@
 #define GO_BACK goto a1;
 #define CONDITION(n) if(n)
 
-
-
-/*int main()
-{
-    int a[20];
-    int i, n;
-    int cnt = 0;
-
-    setlocale(LC_ALL, "Russian");
-    printf("\nВведите длину массива >> ");
-    scanf_s("%d", &n);
-    if (n > 20 || n < 5)
-    {
-        printf("\nERROR");
-    }
-    for (i = 0; i < n; i++)
-    {
-    a1:printf("\nВведите значение элемента a[%d] >> ", i);
-        scanf_s("%d", &a[i]);
-        if (a[i] > 50)
-        {
-            printf("\nЧисло не входит в диапазон допустимых значений (-50 - 50)");
-            goto a1;
-        }
-        if (a[i] < -50)
-        {
-            printf("\nЧисло не входит в диапазон допустимых значений (-50 - 50)");
-            goto a1;
-        }
-        if (a[i] == a[i - 1])
-        {
-            cnt++;
-        }
-    }
-    printf("%d", cnt);
-}*/
-
 int main()
 {
     int a[20];
